3cbcf7b27c7d226fabac29576f36a4b9eeafe5e3
Liam <yelopartyhat@gmail.com>
2022-03-08 15:43:15 +1000

    seperating react app into client folder, added express server to help handle backend stripe payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 import CrownSVG from '../../assets/crown.svg';
 
@@ -9,8 +10,18 @@ const StripeCheckoutButton = ({ price }) => {
     const publishableKey = 'pk_test_eDE3S2MoYCEj8gJ0s5YyO1HT00wpYVDoFt';
 
     const onToken = token => {
-        console.log(token);
-        alert("Payment successfully processed!");
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token,
+            },
+        }).then(response => { alert("Payment successful!") })
+          .catch(error => { 
+              console.error("Payment error: ", JSON.parse(error));
+              alert('There was an issue with your payment. Please ensure you are using the provided credit card details.');
+            });
     }
 
     return (
---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const res = require('express/lib/response');
+
+// Load config.
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Json decoode all requests, set urlencoded to true.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+if(process.env.NODE_ENV === 'production') {
+    // Server static files if in production
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    // On request, server index.html from client/build folder while in production.
+    app.get('*', function(request, response) {
+        response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
+// Listen for errors
+app.listen(port, error => {
+    if(error) throw error;
+    console.log("Server running on port " + port);
+})
+
+// When a request gets sent to /payment endpoint...
+app.post('/payment', (request, response) => {
+    const body = {
+        source: request.body.token.id,
+        amount: request.body.amount,
+        currency: 'aud',
+    };
+
+    // Respond with error or stripe response.
+    stripe.charges.create(body, (stripeError, stripeResponse) => {
+        if(stripeError)
+            response.status(500).send({ error: stripeError });
+        else
+            response.status(200).send({ success: stripeResponse });
+    })
+})
